e034fd61f754a64e22b211923f024992ecf072da
Jon Daniel <joneqdaniel@gmail.com>
2026-07-14 23:10:04 +0000

    Initial commit

---
+++ include/sys/fps.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <cstdlib>
+#include <cstddef>
+#include <cstdint>
+#include <chrono>
+#include <cstring>
+
+namespace sys
+{
+	struct timer	
+	{
+		struct date
+		{
+			std::time_t now = 0;
+			char str[32];
+		} date;
+		struct fps
+		{
+			std::size_t cnt = 0;
+			char str[32];
+		} fps;
+		char str[64];
+		using clock = std::chrono::high_resolution_clock;
+		std::chrono::time_point<clock> pre = clock::now();
+		std::chrono::time_point<clock> now = clock::now();
+		std::size_t acc = 0;
+		std::size_t dif = std::chrono::duration_cast<std::chrono::nanoseconds>(now - pre).count();
+
+		bool step(const std::size_t len = 1000000000)
+		{
+			static bool first = true;
+			pre  = now;
+			now  = clock::now();
+			dif  = std::chrono::duration_cast<std::chrono::nanoseconds>(now - pre).count();
+			acc += dif;
+
+			if(first)
+			{
+				snprintf(fps.str,31, "%zu", fps.cnt);
+				date.now = std::chrono::system_clock::to_time_t(now);
+				ctime_r(&date.now, date.str); date.str[strlen(date.str) - 1] = '\0';
+			}
+			if(acc >= len)
+			{
+				snprintf(fps.str,31, "%zu", fps.cnt);
+				date.now = std::chrono::system_clock::to_time_t(now);
+				ctime_r(&date.now, date.str); date.str[strlen(date.str) - 1] = '\0';
+				fps.cnt = 0;
+				acc -= len;
+				if(first) first = false;
+			}
+			else 
+				fps.cnt++;
+			return true;
+		}
+		char* to_string()
+		{
+			snprintf(str, 64, "%s %8zu/%s", date.str, fps.cnt, fps.str);
+			return str;
+		}
+	} timer;
+};
---
+++ include/sys/kbd.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <fcntl.h>
+#include <sys/param.h>
+#include <unistd.h>
+#include <termios.h>
+
+namespace sys::term
+{
+	void halt()
+	{
+		struct termios info;
+		int flags = fcntl(STDIN_FILENO,F_GETFL,0);
+		fcntl(STDIN_FILENO, F_SETFL,flags | ~O_NONBLOCK);
+		tcgetattr(STDIN_FILENO, &info);
+		info.c_lflag |= ICANON;
+		info.c_lflag |= ECHO;
+		tcsetattr(STDIN_FILENO, TCSANOW, &info);
+		puts("");
+	}
+	bool init()
+	{
+		struct termios info;
+		tcgetattr(STDIN_FILENO, &info);
+		info.c_lflag &= ~ICANON;
+		info.c_lflag &= ~ECHO;
+		info.c_cc[VMIN] = 1;
+		info.c_cc[VTIME] = 0;
+		tcsetattr(STDIN_FILENO, TCSANOW, &info);
+		int flags = fcntl(STDIN_FILENO,F_GETFL,0);
+		if (flags == -1)
+		{
+			perror("fcntl: F_GETFL failed");
+			halt();
+			return false;
+		}
+		if(fcntl(STDIN_FILENO, F_SETFL,flags | O_NONBLOCK) == -1)
+		{
+			perror("fcntl: F_SETFL failed");
+			halt();
+			return false;
+		}
+		atexit(sys::term::halt);
+
+		return true;
+	}
+	bool feed()
+	{
+		int c = '\0';
+		if(read(STDIN_FILENO, &c, 1) < 0 && errno != EAGAIN && errno != EWOULDBLOCK)
+		{
+			perror("fgetc: failed\n");
+			exit(EXIT_FAILURE);
+		}
+		return c != 27;
+	}
+};
